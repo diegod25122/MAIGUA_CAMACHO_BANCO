40ba0537af163db41e3f83e3fae2d2a0b051a55e
diegod25122 <diego.camacho@epn.edu.ec>
2025-12-14 18:21:02 -0500

    Deber con bdd Bancario

--- src/db/Conexion.java
+++ src/db/Conexion.java
@@ -1,4 +1,20 @@
 package db;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class Conexion {
+
+    private static final String URL = "jdbc:mysql://localhost:3306/banco_db";
+    private static final String USER = "root";
+    private static final String PASS = "root"; // cambia esto
+
+    public static Connection getConexion() {
+        try {
+            return DriverManager.getConnection(URL, USER, PASS);
+        } catch (Exception e) {
+            System.out.println("Error BD: " + e.getMessage());
+            return null;
+        }
+    }
 }
--- src/forms/Loginform.java
+++ src/forms/Loginform.java
@@ -1,4 +1,71 @@
 package forms;
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javax.swing.*;
+import db.Conexion;
+import java.sql.*;
 
-public class Loginform {
+public class LoginForm extends JFrame {
+
+    private JTextField txtUsuario;
+    private JPasswordField txtClave;
+    private JButton btnIngresar;
+
+    public LoginForm() {
+        setTitle("Inicio de Sesión");
+        setSize(320, 180);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        initComponents();
+    }
+
+    private void initComponents() {
+        JLabel lblU = new JLabel("Usuario:");
+        JLabel lblC = new JLabel("Contraseña:");
+
+        txtUsuario = new JTextField(15);
+        txtClave = new JPasswordField(15);
+        btnIngresar = new JButton("Ingresar");
+
+        btnIngresar.addActionListener(e -> validarLogin());
+
+        JPanel panel = new JPanel();
+        panel.add(lblU);
+        panel.add(txtUsuario);
+        panel.add(lblC);
+        panel.add(txtClave);
+        panel.add(btnIngresar);
+
+        add(panel);
+    }
+
+    private void validarLogin() {
+        String user = txtUsuario.getText();
+        String pass = new String(txtClave.getPassword());
+        try {
+            Connection cn = Conexion.getConexion();
+
+            String sql = "SELECT * FROM usuarios WHERE username=? AND password=? AND activo=true";
+            PreparedStatement ps = cn.prepareStatement(sql);
+            ps.setString(1, user);
+            ps.setString(2, pass);
+
+            ResultSet rs = ps.executeQuery();
+
+            if (rs.next()) {
+                JOptionPane.showMessageDialog(this, "Bienvenido " + user);
+                new BancoForm(user).setVisible(true);
+                this.dispose();
+            } else {
+                JOptionPane.showMessageDialog(this, "Credenciales incorrectas");
+            }
+
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(this, "Error conexión BD");
+        }
+    }
 }
+
